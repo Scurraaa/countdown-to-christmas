5bb40bb326c47c0e67a96f1026c06451028a1845
Joshua Bacani <joshua.bacani12@gmail.com>
2021-04-18 12:50:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const daysEl = document.getElementsByClassName('days-value')[0];
+const hoursEl = document.getElementsByClassName('hours-value')[0];
+const minuteEl = document.getElementsByClassName('minute-value')[0];
+const secondsEl = document.getElementsByClassName('seconds-value')[0];
+
+const christmas = '25 Dec 2021';
+
+console.log(daysEl);
+
+function countdown() {
+    const chirstmasDate = new Date(christmas)
+    const currentDate = new Date()
+
+    var differenceDate = chirstmasDate - currentDate;
+
+    const totalSeconds = (chirstmasDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    const hours = Math.floor(totalSeconds / 3600) % 24; 
+
+    const minute = Math.floor(totalSeconds / 3600 / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minuteEl.innerHTML = minute;
+    secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
